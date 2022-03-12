c276a7185575b20bff927273dd55d4fdd37db74f
Alena Makhaeva <lemah@mail.ru>
2022-03-12 17:59:22 +0300

    Edit idents and tabs

--- lesson_2/basket.rb
+++ lesson_2/basket.rb
@@ -12,34 +12,34 @@ loop do
 	print "наименование: "
 	title = gets.chomp
 	break if title.capitalize == "Стоп"
-  if hash_basket[title.to_sym].nil?
-	print "цена: "
-	pr = gets.chomp.to_f
-	print "количество: "
-	quant = gets.chomp.to_f
+	if hash_basket[title.to_sym].nil?
+		print "цена: "
+		pr = gets.chomp.to_f
+		print "количество: "
+		quant = gets.chomp.to_f
 
-	hash_basket[title.to_sym] = {price: pr, quantity: quant}
-    puts
-  else 
-    puts "Данный товар уже есть в корзине. "
-    puts "Добавить еще некоторое количество данного товара (да/нет)?"
-    answer = gets.chomp.downcase
-    if answer == "да"
-      print "количество: "
-	  quant = gets.chomp.to_f
-      hash_basket[title.to_sym][:quantity] += quant
-      puts
-    end
-  end
-end 
+		hash_basket[title.to_sym] = {price: pr, quantity: quant}
+		puts
+	else
+		puts "Данный товар уже есть в корзине. "
+		puts "Добавить еще некоторое количество данного товара (да/нет)?"
+		answer = gets.chomp.downcase
+		if answer == "да"
+			print "количество: "
+			quant = gets.chomp.to_f
+			hash_basket[title.to_sym][:quantity] += quant
+			puts
+		end
+	end
+end
 
 total_cost = 0
 
 hash_basket.each do |k, hv|
-  cost = 0
+	cost = 0
 	print "#{k}: "
 	puts "#{hv[:price]} руб. * #{hv[:quantity]} шт.  =  #{cost += (hv[:price] * hv[:quantity]).round(2)} руб."
-  total_cost += cost
+	total_cost += cost
 end
 
 puts
